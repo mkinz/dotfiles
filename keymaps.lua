1c42b89cca301a66b605910f1d0235ebddf66a7c
Matt <mkinz@users.noreply.github.com>
2024-08-05 17:23:48 -0400

    nvim lua keymaps

---
+++ keymaps.lua
@@ -0,0 +1,20 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+vim.keymap.set("x", "<leader>p", [["_dP]])
+
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+vim.keymap.set("n", "j", "h")
+vim.keymap.set("n", "k", "j")
+vim.keymap.set("n", "l", "k")
+vim.keymap.set("n", ";", "l")
+vim.keymap.set("v", "j", "h")
+vim.keymap.set("v", "k", "j")
+vim.keymap.set("v", "l", "k")
+vim.keymap.set("v", ";", "l")
